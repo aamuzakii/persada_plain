5e0a1010465ed43fc5e4621d54dac770a0cef036
zaki <abdullah.muzaki@alodokter.com>
2022-05-26 17:45:49 +0700

    delete cors_set_access_control_headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   require 'json_web_token'
 
   skip_before_action :verify_authenticity_token
-  # after_action :cors_set_access_control_headers
 
   def authorize_request
     token = request.cookies['token']
@@ -12,13 +11,5 @@ class ApplicationController < ActionController::Base
     rescue
     end
   end
-
-  # def cors_set_access_control_headers
-  #   headers['Access-Control-Allow-Origin'] = 'https://www.dev.persada.store/'
-  #   headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET'
-  #   headers['Access-Control-Request-Method'] = '*'
-  #   headers['Access-Control-Allow-Credentials'] = 'true'
-  #   headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  # end
   
 end

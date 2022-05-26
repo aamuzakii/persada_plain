d5e95e7485b7b8e28851164014deb5d8d3073d24
zaki <abdullah.muzaki@alodokter.com>
2022-05-26 16:48:13 +0700

    comment cors_set_access_control_headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   require 'json_web_token'
 
   skip_before_action :verify_authenticity_token
-  after_action :cors_set_access_control_headers
+  # after_action :cors_set_access_control_headers
 
   def authorize_request
     token = request.cookies['token']
@@ -13,12 +13,12 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = 'http://localhost:8000'
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET'
-    headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Credentials'] = 'true'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  end
+  # def cors_set_access_control_headers
+  #   headers['Access-Control-Allow-Origin'] = 'http://localhost:8000'
+  #   headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET'
+  #   headers['Access-Control-Request-Method'] = '*'
+  #   headers['Access-Control-Allow-Credentials'] = 'true'
+  #   headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  # end
   
 end
